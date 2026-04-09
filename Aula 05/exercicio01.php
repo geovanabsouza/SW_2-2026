38999b544ea57282b290f1a18e7b2b32e9b8795d
Geovana Souza <159726975+geovanabsouza@users.noreply.github.com>
2026-04-09 08:46:53 -0300

    Delete Aula 05/exercicio01.php

--- Aula 05/exercicio01.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $json_str = '{"Nome:": "Caderno Inteligente", "Preço:": 89.90, "Quantidade:" : 57}'; 
-    $obj = json_decode($json_str);
-
-    echo"<pre>";
-    var_dump ($obj);
-    echo"<pre>";
-?>
